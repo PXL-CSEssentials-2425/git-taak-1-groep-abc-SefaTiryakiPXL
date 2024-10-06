283a3b7af1c19ea2f1ce00b0772e0930577af15d
Sefa Tiryaki <12401207@student.pxl.be>
2024-10-06 15:40:34 +0200

    KLEUR-01 Hoofdscherm De achtergrondkleur van het scherm is licht grijs. Er is een TextBlock met de titel van de applicatie te zien (“Kleurenpsychologie”).

---
+++ Git_taak_1/Git_taak_1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Git_taak_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PXLpng_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Background = PxlPhoto.Source != null ? new SolidColorBrush(Colors.LightBlue) : new SolidColorBrush(Colors.LightGray);
+        }
+
+        // MouseLeave = event for the PXL Image (Achtergrond)
+        private void PXLpng_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.LightGray);
+        }
+    }
+}
