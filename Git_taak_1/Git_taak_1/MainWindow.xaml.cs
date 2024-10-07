dccb7d409b34275c3628519d16836f41daccba77
Sefa Tiryaki <12401207@student.pxl.be>
2024-10-07 16:02:41 +0200

    KLEUR-01 Hoofdscherm De achtergrondkleur van het scherm is licht grijs. Er is een TextBlock met de titel van de applicatie te zien (“Kleurenpsychologie”).

--- Git_taak_1/Git_taak_1/MainWindow.xaml.cs
+++ Git_taak_1/Git_taak_1/MainWindow.xaml.cs
@@ -25,44 +25,5 @@ namespace Git_taak_1
         {
             InitializeComponent();
         }
-        // onderste 2 zijn de hover effect
-        private void PXLpng_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            this.Background = PxlPhoto.Source != null ? new SolidColorBrush(Colors.LightBlue) : new SolidColorBrush(Colors.LightGray);
-        }
-
-        // MouseLeave = event for the PXL Image (Achtergrond)
-        private void PXLpng_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            this.Background = new SolidColorBrush(Colors.LightGray);
-        }
-        private void KleurButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == roodButton)
-            {
-                ShowColorInfo("#FF0000", "Rood is de kleur van warmte.", "Rood.png");
-            }
-            else if (sender == groenButton)
-            {
-                ShowColorInfo("#008000", "Groen is de kleur van genezing.", "Groen.png");
-            }
-            else if (sender == blauwButton)
-            {
-                ShowColorInfo("#0000FF", "Blauw is de kleur van intelligentie.", "Blauw.png");
-            }
-            else if (sender == geelButton)
-            {
-                ShowColorInfo("#FFFF00", "Geel is de kleur van levenslust.", "Geel.png");
-            }
-
-        }
-
-        // Help functie voor de kleuren te updaten en informatie 
-        private void ShowColorInfo(string colorCode, string colorInfo, string imagePath)
-        {
-            ColorCodeText.Text = $"Kleurcode: {colorCode}";
-            ColorInfoText.Text = colorInfo;
-            ColorImageDisplay.Source = new BitmapImage(new Uri(imagePath, UriKind.Relative));
-        }
     }
 }
